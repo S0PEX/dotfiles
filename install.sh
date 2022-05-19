66e498e56ba40d466c899d0c657ea6276cd97f2b
Artur Komaristych <artur@s0pex.me>
2022-05-19 19:57:58 +0200

    chore: inital commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BAR="polybar"
+MISC="feh st pcmanfm stow slock xorg-xsetroot" # feh (wallpaper) st(terminal) pcmanfm(file explorer) stow(handle dotfiles linking), slock(lock screen)
+LAUNCHER="rofi"
+COMPOSER="picom"
+NOTIFICATION="dunst"
+WINDOW_MANAGER="bspwm sxhkd"
+
+# Install required packages
+sudo pacman -S $BAR $MISC $LAUNCHER $COMPOSER $NOTIFICATION $WINDOW_MANAGER
+
+# Link this repo directories to $HOME/.config
+echo "Linking basic configuration"
+stow default
+
+echo "Done."
